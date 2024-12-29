fcf7caa4e08a1569a3276e3daf1d42619f9cba4c
Rui Han <chen_ruihan@u.nus.edu>
2024-12-29 14:59:31 +0800

    Change env settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-dotenv.config()
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode
+  const env = loadEnv(mode, process.cwd());
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
-})
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    }
+  };
+});
