69331270b9223080270b50ce169d06bf2c71b1d4
Rui Han <chen_ruihan@u.nus.edu>
2025-01-04 02:23:53 +0800

    Add notice to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,24 @@
-import { Text } from "@chakra-ui/react";
+import { Link, Text } from "@chakra-ui/react";
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export default function TwoPhasePage() {
 
   return (
-    <Text fontSize={"2xl"} fontWeight={"bold"}>
-      Home
+    <>
+      <Text fontSize={"2xl"} fontWeight={"bold"}>
+        Home.
+      </Text>
+
+      <Text fontSize={"xl"} fontWeight={"bold"}>
+        Website under construction, but the tableau calculators in the navigation menu work.
+        <br />
+        Source code hosted at{' '}
+          <Link href="https://github.com/rui-han-crh/simplexalgo-frontend" target="_blank" rel="noopener noreferrer">
+            https://github.com/rui-han-crh/simplexalgo-frontend
+          </Link>
+        .
     </Text>
+    </>
   );
 }
